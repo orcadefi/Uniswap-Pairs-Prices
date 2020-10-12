47253a2bb256651495fb265d78b331e9e9d58a64
orcadefi <dev@orcadefi.com>
2020-10-11 23:17:18 -0500

    Documentation update

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
 # Uniswap-Pairs-Prices
- Simple pair ticker with Uniswap SDK
+
+Uniswap pair price is a Javascript project for get price oracles in realtime data with the Uniswap official SDK.
+
+Supported platforms and pairs:
+- Uniswap
+  - DAI/ETH
+  - USDT/ETH
+  - USDC/ETH
+  - LEND/ETH
+  - MKR/ETH
+  - REN/ETH
+  - SUSD/ETH
+  - KNC/ETH
+  - BAT/ETH
+  - TUSD/ETH
+  - LINK/ETH
+  - REP/ETH
+  - ZRX/ETH
+  - SNX/ETH,
+  - MANA/ETH
+  - WBTC/ETH
+  - ENJ/ETH
+  - YFI/ETH
+ 
+## Installation
+
+Use the git to clone this repository.
+
+```bash
+git clone https://github.com/orcadefi/Uniswap-Pairs-Prices.git
+cd Uniswap-Pairs-Prices
+```
+
+Install the modules and dependencies:
+```bash
+npm install
+```
+
+## Usage
+
+Get the price of the pairs of Uniswap platform:
+
+```bash
+npm run start
+```
+
+#### Example output
+```
+DAI/ETH   : 0.00269516 ETH
+USDT/ETH  : 0.000000000374174 ETH
+USDC/ETH  : 0.000000000374706 ETH
+LEND/ETH  : 0.00136011 ETH
+MKR/ETH   : 1.57255 ETH
+REN/ETH   : 0.00104054 ETH
+SUSD/ETH  : 0.00268578 ETH
+KNC/ETH   : 0.00256428 ETH
+BAT/ETH   : 0.000596398 ETH
+TUSD/ETH  : 0.00267829 ETH
+LINK/ETH  : 0.0289819 ETH
+REP/ETH   : 0.0382378 ETH
+ZRX/ETH   : 0.00104702 ETH
+SNX/ETH   : 0.0111449 ETH
+MANA/ETH  : 0.000210725 ETH
+WBTC/ETH  : 30.4641000000 ETH
+ENJ/ETH   : 0.000429558 ETH
+YFI/ETH   : 43.9652 ETH
+```
+
+# Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const UNISWAP = require('@uniswap/sdk')
+//Import the Uniswap SDK
+const UNISWAP = require('@uniswap/sdk');
 
-//Tokens
-const USDC = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 18,'USDC','USD Coin')
-const DAI = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18,'DAI','Dai')
-const USDT = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0xdAC17F958D2ee523a2206206994597C13D831ec7', 18,'USDT','usdt')
-const LEND = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x80fB784B7eD66730e8b1DBd9820aFD29931aab03', 18,'LEND','lend')
-const MKR = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2', 18,'MKR','mkr')
-const REN = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x408e41876cCCDC0F92210600ef50372656052a38', 18,'REN','ren')
-const SUSD = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x57Ab1ec28D129707052df4dF418D58a2D46d5f51', 18,'SUSD','susd')
-const KNC = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0xdd974D5C2e2928deA5F71b9825b8b646686BD200', 18,'KNC','knc')
-const BAT = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x0D8775F648430679A709E98d2b0Cb6250d2887EF', 18,'BAT','bat')
-const TUSD = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x0000000000085d4780B73119b644AE5ecd22b376', 18,'TUSD','tusd')
-const LINK = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x514910771AF9Ca656af840dff83E8264EcF986CA', 18,'LINK','link')
-const REP = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x1985365e9f78359a9B6AD760e32412f4a445E862', 18,'REP','rep')
-const ZRX = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0xE41d2489571d322189246DaFA5ebDe1F4699F498', 18,'ZRX','zrx')
-const SNX = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F', 18,'SNX','snx')
-const MANA = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x0F5D2fB29fb7d3CFeE444a200298f468908cC942', 18,'MANA','mana')
-const WBTC = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599', 18,'wbtc','wbtc')
-const ENJ = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0xF629cBd94d3791C9250152BD8dfBDF380E2a3B9c', 18,'ENJ','enj')
-const YFI = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e', 18,'YFI','yfi')
+//Tokens with the address and the symbols
+const USDC = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 18,'USDC','USD Coin');
+const DAI = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18,'DAI','Dai');
+const USDT = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0xdAC17F958D2ee523a2206206994597C13D831ec7', 18,'USDT','usdt');
+const LEND = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x80fB784B7eD66730e8b1DBd9820aFD29931aab03', 18,'LEND','lend');
+const MKR = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2', 18,'MKR','mkr');
+const REN = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x408e41876cCCDC0F92210600ef50372656052a38', 18,'REN','ren');
+const SUSD = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x57Ab1ec28D129707052df4dF418D58a2D46d5f51', 18,'SUSD','susd');
+const KNC = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0xdd974D5C2e2928deA5F71b9825b8b646686BD200', 18,'KNC','knc');
+const BAT = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x0D8775F648430679A709E98d2b0Cb6250d2887EF', 18,'BAT','bat');
+const TUSD = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x0000000000085d4780B73119b644AE5ecd22b376', 18,'TUSD','tusd');
+const LINK = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x514910771AF9Ca656af840dff83E8264EcF986CA', 18,'LINK','link');
+const REP = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x1985365e9f78359a9B6AD760e32412f4a445E862', 18,'REP','rep');
+const ZRX = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0xE41d2489571d322189246DaFA5ebDe1F4699F498', 18,'ZRX','zrx');
+const SNX = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F', 18,'SNX','snx');
+const MANA = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x0F5D2fB29fb7d3CFeE444a200298f468908cC942', 18,'MANA','mana');
+const WBTC = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599', 18,'wbtc','wbtc');
+const ENJ = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0xF629cBd94d3791C9250152BD8dfBDF380E2a3B9c', 18,'ENJ','enj');
+const YFI = new UNISWAP.Token(UNISWAP.ChainId.MAINNET, '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e', 18,'YFI','yfi');
 
 
-//Get Price
+//Asynchronous function to retrieve the pairs price
 async function pairs() {
+    //All pairs in ETH price
     let pairs = [
         "DAI/ETH",
         "USDT/ETH",
@@ -44,6 +46,7 @@ async function pairs() {
         "YFI/ETH",
     ]
     let pair = [], route = [];
+    //Pair declarations
     pair[0] = await UNISWAP.Fetcher.fetchPairData(DAI, UNISWAP.WETH[DAI.chainId])
     pair[1] = await UNISWAP.Fetcher.fetchPairData(USDT, UNISWAP.WETH[USDT.chainId])
     pair[2] = await UNISWAP.Fetcher.fetchPairData(USDC, UNISWAP.WETH[USDC.chainId])
@@ -62,6 +65,7 @@ async function pairs() {
     pair[15] = await UNISWAP.Fetcher.fetchPairData(WBTC, UNISWAP.WETH[WBTC.chainId])
     pair[16] = await UNISWAP.Fetcher.fetchPairData(ENJ, UNISWAP.WETH[ENJ.chainId])
     pair[17] = await UNISWAP.Fetcher.fetchPairData(YFI, UNISWAP.WETH[YFI.chainId])
+    //Route declarations
     route[0] = new UNISWAP.Route([pair[0]], UNISWAP.WETH[DAI.chainId])
     route[1] = new UNISWAP.Route([pair[1]], UNISWAP.WETH[USDT.chainId])
     route[2] = new UNISWAP.Route([pair[2]], UNISWAP.WETH[USDC.chainId])
@@ -80,14 +84,15 @@ async function pairs() {
     route[15] = new UNISWAP.Route([pair[15]], UNISWAP.WETH[WBTC.chainId])
     route[16] = new UNISWAP.Route([pair[16]], UNISWAP.WETH[ENJ.chainId])
     route[17] = new UNISWAP.Route([pair[17]], UNISWAP.WETH[YFI.chainId])
-
+    //Retrieve each pair data and transform to significant data
     for (let i = 0; i < 18; i++) {
         if (i === 1 || i === 2){
-            console.log(pairs[i], " :",route[i].midPrice.toSignificant(6)) // 201.306
+            console.log(pairs[i], " :",route[i].midPrice.toSignificant(6), " ETH") // 201.306
         }else{
-            console.log(pairs[i], " :",route[i].midPrice.invert().toSignificant(6)) // 201.306
+            console.log(pairs[i], " :",route[i].midPrice.invert().toSignificant(6) ," ETH") // 201.306
         }
     }
 }
+//Call the pairs data and print the output
 pairs().then();
 
